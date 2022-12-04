1dc0431b4896a4a7e568cb12eaecf18fdf4d2378
FinnbarHome <2481383@dundee.ac.uk>
2022-12-04 00:53:15 +0000

    Committed initial sim file
    Empty sim file with absolute bare minimum in it

---
+++ simulator.cpp
@@ -0,0 +1,18 @@
+/*
+	GROUP 1 MANCHESTER BABY SIMULATOR
+	Names: Finnbar Home, Abz Mohamed, Samuel Tweedie, Oliver Shearer, John Nicol, Kayee Liu
+	Module Code: AC21009
+*/
+
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+	
+}
+
+
+
+
