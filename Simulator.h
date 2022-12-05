79bb0fa2ca397f169e6d7d1a8ac2eac171a54505
FinnbarHome <2481383@dundee.ac.uk>
2022-12-05 01:17:41 +0000

    Re-added my old stuff
    Re-added stuff that was removed and commented stuff that has been misplaced (TEMPORARY)

--- Simulator.h
+++ Simulator.h
@@ -30,341 +30,354 @@ class Simulator
         int sizeOfMemory = 32;
         int sizeOfMemLoca = 32;
 
-	public:
+	public: 
+        void increment_CI();
+		void fetch();
+		void decode();
+		void execute();
+		void JMP();
+		void JRP();
+		void LDN();
+		void SUB();
+		void STO();
+		void CMP();
+		void STP();
+		void display_everything();
 
-        vector<int> getAccumulator()
-        {
-            return accumulator;
-        }
 
-        vector<int> getCI()
-        {
-            return CI;
-        }
+        // vector<int> getAccumulator()
+        // {
+        //     return accumulator;
+        // }
 
-        vector<int> getPI()
-        {
-            return PI;
-        }
+        // vector<int> getCI()
+        // {
+        //     return CI;
+        // }
 
-        vector<vector<int>> getMemory()
-        {
-            return memory;
-        }
+        // vector<int> getPI()
+        // {
+        //     return PI;
+        // }
 
-        int getInstructionSet()
-        {
-            return currentInstructionSet;
-        }
+        // vector<vector<int>> getMemory()
+        // {
+        //     return memory;
+        // }
 
-        void setAccumulator(vector<int> accumulator)
-        {
-            this->accumulator = accumulator;
-        }
+        // int getInstructionSet()
+        // {
+        //     return currentInstructionSet;
+        // }
 
-        void setCI(vector<int> ci)
-        {
-            this->CI = ci;
-        }
+        // void setAccumulator(vector<int> accumulator)
+        // {
+        //     this->accumulator = accumulator;
+        // }
 
-        void setPI(vector<int> pi)
-        {
-            this->PI = pi;
-        }
+        // void setCI(vector<int> ci)
+        // {
+        //     this->CI = ci;
+        // }
 
-        void setMemorySize(int num)
-        {
-            this->sizeOfMemory = num;
-            memory.clear();
-        }
+        // void setPI(vector<int> pi)
+        // {
+        //     this->PI = pi;
+        // }
 
-        void setMemLocaSize(int num)
-        {
-            this->sizeOfMemLoca = num;
-            memory.clear();
-        }
+        // void setMemorySize(int num)
+        // {
+        //     this->sizeOfMemory = num;
+        //     memory.clear();
+        // }
 
-        void setInstructionSet(int set)
-        {
-            this->currentInstructionSet = set;
-        }
+        // void setMemLocaSize(int num)
+        // {
+        //     this->sizeOfMemLoca = num;
+        //     memory.clear();
+        // }
 
-		// Read from file
-		bool readFromFile()
-		{
-		    ifstream f;
+        // void setInstructionSet(int set)
+        // {
+        //     this->currentInstructionSet = set;
+        // }
 
-		    // Iterate through ONLY .txt files in current directory
-            DIR *di;
-            char *ptr1,*ptr2;
-            int retn;
-            struct dirent *dir;
-            di = opendir("."); // Open current directory
-            if (di)
-            {
-                while ((dir = readdir(di)) != NULL)
-                {
-                    ptr1 = strtok(dir->d_name, ".");
-                    ptr2 = strtok(NULL, ".");
-                    if (ptr2!=NULL)
-                    {
-                        retn = strcmp(ptr2, "txt");
-                        if(retn == 0)
-                        {
-                            printf("%s.txt\n", ptr1);
-                        }
-                    }
-                }
-                closedir(di);
-            }
-        	cout << "\nWhich machine code file would you like to read in? (include .txt at end): ";
-        	string c;
-        	cin >> c;
-        	cout << c << endl;
-		    f.open(c);
+		// // Read from file
+		// bool readFromFile()
+		// {
+		//     ifstream f;
 
-		    if(!f.is_open()) 
-		    {
-		    	cout << "Could not open file of that name, or does not exist." << endl;
-		    	// clear input buffer
-                cin.clear();
-                cin.ignore(numeric_limits<streamsize>::max(), '\n');
-                return false;
-		    }
+		//     // Iterate through ONLY .txt files in current directory
+        //     DIR *di;
+        //     char *ptr1,*ptr2;
+        //     int retn;
+        //     struct dirent *dir;
+        //     di = opendir("."); // Open current directory
+        //     if (di)
+        //     {
+        //         while ((dir = readdir(di)) != NULL)
+        //         {
+        //             ptr1 = strtok(dir->d_name, ".");
+        //             ptr2 = strtok(NULL, ".");
+        //             if (ptr2!=NULL)
+        //             {
+        //                 retn = strcmp(ptr2, "txt");
+        //                 if(retn == 0)
+        //                 {
+        //                     printf("%s.txt\n", ptr1);
+        //                 }
+        //             }
+        //         }
+        //         closedir(di);
+        //     }
+        // 	cout << "\nWhich machine code file would you like to read in? (include .txt at end): ";
+        // 	string c;
+        // 	cin >> c;
+        // 	cout << c << endl;
+		//     f.open(c);
 
-			string line;
-		    while(getline(f, line)) 
-		    {
-		    	// Insert each character of each line into a vector
-		    	vector<int> v;
-		    	for (int n = 0; n < (line.length()-1); n++)
-		    	{
-		    		char c = line[n];
-		    		int temp = c - '0'; // Conversion to int
+		//     if(!f.is_open()) 
+		//     {
+		//     	cout << "Could not open file of that name, or does not exist." << endl;
+		//     	// clear input buffer
+        //         cin.clear();
+        //         cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        //         return false;
+		//     }
 
-		    		if (temp != 0 && temp != 1)
-		    		{
-		    			f.close();
-		    			v.clear();
-		    			memory.clear();
-                        // clear input buffer
-                        cin.clear();
-                        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		// 	string line;
+		//     while(getline(f, line)) 
+		//     {
+		//     	// Insert each character of each line into a vector
+		//     	vector<int> v;
+		//     	for (int n = 0; n < (line.length()-1); n++)
+		//     	{
+		//     		char c = line[n];
+		//     		int temp = c - '0'; // Conversion to int
 
-		    			return false;
-		    		}
+		//     		if (temp != 0 && temp != 1)
+		//     		{
+		//     			f.close();
+		//     			v.clear();
+		//     			memory.clear();
+        //                 // clear input buffer
+        //                 cin.clear();
+        //                 cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+		//     			return false;
+		//     		}
 		    		
-		    		v.push_back(temp); 
-		    	}
-		    	memory.push_back(v); // Push vector into memory
-		    }
+		//     		v.push_back(temp); 
+		//     	}
+		//     	memory.push_back(v); // Push vector into memory
+		//     }
 
-		    f.close();
+		//     f.close();
 
-		    // for(int i = 0; i < memory.size(); i++)
-		    // {
-		    // 	cout << "MEMORY(i): " << memory.size() << endl;
-		    // 	for (int j = 0; j < memory[i].size(); j++)
-		    // 	{
-		    // 		if (j == 0)
-		    // 			cout << "MEMORY(j): " << memory[j].size() << endl;
-		    // 		cout << memory[i][j];
-		    // 	}
-		    // 	cout <<endl;
-		    // }
+		//     // for(int i = 0; i < memory.size(); i++)
+		//     // {
+		//     // 	cout << "MEMORY(i): " << memory.size() << endl;
+		//     // 	for (int j = 0; j < memory[i].size(); j++)
+		//     // 	{
+		//     // 		if (j == 0)
+		//     // 			cout << "MEMORY(j): " << memory[j].size() << endl;
+		//     // 		cout << memory[i][j];
+		//     // 	}
+		//     // 	cout <<endl;
+		//     // }
 
-		    return true;
-		}
+		//     return true;
+		// }
 
-		// Decode instructions
-		void decode()
-		{
-            CI.assign(32,0);
+		// // Decode instructions
+		// void decode()
+		// {
+        //     CI.assign(32,0);
 
-            while(lamp == false)
-            {
-                int dec = binaryToDec(CI);
-                PI = memory[dec];
-                vector<int> opc;
-                if(currentInstructionSet == 3)
-                {
-                    opc = {PI[13],PI[14],PI[15]};
-                    opcode(opc);
-                }
-                else if(currentInstructionSet == 4)
-                {
-                    opc = {PI[13],PI[14],PI[15],PI[16]};
-                    opcode(opc);
-                }
-                else
-                {
-                    opc = {PI[13],PI[14],PI[15],PI[16],PI[17]};
-                    opcode(opc);
-                }
+        //     while(lamp == false)
+        //     {
+        //         int dec = binaryToDec(CI);
+        //         PI = memory[dec];
+        //         vector<int> opc;
+        //         if(currentInstructionSet == 3)
+        //         {
+        //             opc = {PI[13],PI[14],PI[15]};
+        //             opcode(opc);
+        //         }
+        //         else if(currentInstructionSet == 4)
+        //         {
+        //             opc = {PI[13],PI[14],PI[15],PI[16]};
+        //             opcode(opc);
+        //         }
+        //         else
+        //         {
+        //             opc = {PI[13],PI[14],PI[15],PI[16],PI[17]};
+        //             opcode(opc);
+        //         }
 
-                int num = binaryToDec(CI);
-                num += 1;
-                vector<int> memloca = decToBinary(num);
-                CI = memloca;
-            }
+        //         int num = binaryToDec(CI);
+        //         num += 1;
+        //         vector<int> memloca = decToBinary(num);
+        //         CI = memloca;
+        //     }
 
-            lamp = false;
-		}
+        //     lamp = false;
+		// }
 
-		// OPCODE functions (use case statements)
-		void opcode(vector<int> opc)
-		{
-            int num = binaryToDec(opc);
+		// // OPCODE functions (use case statements)
+		// void opcode(vector<int> opc)
+		// {
+        //     int num = binaryToDec(opc);
 
-            switch (num)
-            {
-            case 0: JMP();
-                break;
+        //     switch (num)
+        //     {
+        //     case 0: JMP();
+        //         break;
 
-            case 1: JRP();
-                break;
+        //     case 1: JRP();
+        //         break;
 
-            case 2: LDN();
-                break;
+        //     case 2: LDN();
+        //         break;
 
-            case 3: STO();
-                break;
+        //     case 3: STO();
+        //         break;
 
-            case 4: SUB();
-                break;
+        //     case 4: SUB();
+        //         break;
 
-            case 5: SUB();
-                break;
+        //     case 5: SUB();
+        //         break;
 
-            case 6: CMP();
-                break;
+        //     case 6: CMP();
+        //         break;
 
-            case 7: STP();
-                break;
+        //     case 7: STP();
+        //         break;
             
-            default: break;
-            }
-		}
+        //     default: break;
+        //     }
+		// }
 
-        void JMP()
-        {
+        // void JMP()
+        // {
 
-        }
+        // }
 
-        void JRP()
-        {
+        // void JRP()
+        // {
 
-        }
+        // }
 
-        void LDN()
-        {
+        // void LDN()
+        // {
 
-        }
+        // }
 
-        void STO()
-        {
+        // void STO()
+        // {
 
-        }
+        // }
 
-        void SUB()
-        {
+        // void SUB()
+        // {
 
-        }
+        // }
 
-        void CMP()
-        {
+        // void CMP()
+        // {
 
-        }
+        // }
 
-        void STP()
-        {
+        // void STP()
+        // {
 
-        }
+        // }
 
-        void displayMemory()
-        {
-            for(int i = 0; i < memory.size(); i++)
-            {
-                for (int j = 0; j < memory[i].size(); j++)
-                {
-                    cout << memory[i][j];
-                }
-                cout << endl;
-            }
-        }
+        // void displayMemory()
+        // {
+        //     for(int i = 0; i < memory.size(); i++)
+        //     {
+        //         for (int j = 0; j < memory[i].size(); j++)
+        //         {
+        //             cout << memory[i][j];
+        //         }
+        //         cout << endl;
+        //     }
+        // }
 
-        bool execute()
-        {
-            if (memory.empty())
-                return false;
+        // bool execute()
+        // {
+        //     if (memory.empty())
+        //         return false;
 
-            bool done = false;
-            while (!done)
-            {
-                system("clear"); // Clear screen after every cycle
-                displayMemory();
-                cout << "\nTerminate program by CTRL+C.\n";
-                sleep(1);
-            }
-            return true;
-        }
+        //     bool done = false;
+        //     while (!done)
+        //     {
+        //         system("clear"); // Clear screen after every cycle
+        //         displayMemory();
+        //         cout << "\nTerminate program by CTRL+C.\n";
+        //         sleep(1);
+        //     }
+        //     return true;
+        // }
 
-        // Convert decimal to 32-bit binary equivalent
-        vector<int> decToBinary(int num)
-        {
-            // Store binary as string to ensure initial 0's are not ignored
-            string bin = "";
-            while (num > 0)
-            {
-                bin += to_string(num % 2);
-                num = num / 2;
-            }
+        // // Convert decimal to 32-bit binary equivalent
+        // vector<int> decToBinary(int num)
+        // {
+        //     // Store binary as string to ensure initial 0's are not ignored
+        //     string bin = "";
+        //     while (num > 0)
+        //     {
+        //         bin += to_string(num % 2);
+        //         num = num / 2;
+        //     }
 
-            vector<int> v;
+        //     vector<int> v;
 
-            // For each character in string, convert into int and store in vector
-            for (char ch : bin)
-            {
-                int n = stoi(&ch);
-                v.push_back(n);
-            }
+        //     // For each character in string, convert into int and store in vector
+        //     for (char ch : bin)
+        //     {
+        //         int n = stoi(&ch);
+        //         v.push_back(n);
+        //     }
 
-            return v;
-        }
+        //     return v;
+        // }
 
-        // Convert binary to decimal equivalent
-        int binaryToDec(vector<int> num)
-        {
-            // Store decimal and base
-            int dec = 0, base = 1;
+        // // Convert binary to decimal equivalent
+        // int binaryToDec(vector<int> num)
+        // {
+        //     // Store decimal and base
+        //     int dec = 0, base = 1;
 
-            // Reverse vector to account for big-endian
-            reverse(num.begin(), num.end());
+        //     // Reverse vector to account for big-endian
+        //     reverse(num.begin(), num.end());
 
-            // TEST: Print reversed vector
-            // for (auto element : num)
-            // 	cout << element;
-            // cout << endl;
+        //     // TEST: Print reversed vector
+        //     // for (auto element : num)
+        //     // 	cout << element;
+        //     // cout << endl;
             
-            // Store each element of vector into string
-            stringstream ss;
-            for(int i = 0; i < num.size(); ++i)
-            {
-            ss << num[i];
-            }
-            string s = ss.str();
+        //     // Store each element of vector into string
+        //     stringstream ss;
+        //     for(int i = 0; i < num.size(); ++i)
+        //     {
+        //     ss << num[i];
+        //     }
+        //     string s = ss.str();
 
-            // Convert to binary
-            int temp = stoi(s);
-            while (temp)
-            {
-                int lastDigit = temp % 10;
-                temp = temp / 10;
-                dec += lastDigit * base;
-                base = base * 2;
-            }
+        //     // Convert to binary
+        //     int temp = stoi(s);
+        //     while (temp)
+        //     {
+        //         int lastDigit = temp % 10;
+        //         temp = temp / 10;
+        //         dec += lastDigit * base;
+        //         base = base * 2;
+        //     }
 
-            return dec;
-        }
+        //     return dec;
+        // }
 
 };
--- simulator.cpp
+++ simulator.cpp
@@ -8,15 +8,95 @@
 #include <iostream>
 using namespace std;
 
-// Function declarations
-void displayMenu();
+
+//Adds one to control instruction
+void Simulator::increment_CI(){
+
+}
+
+//Fetchs next instruction from store
+void Simulator::fetch(){
+
+}
+
+//Decodes 5 bit operand and 3 bit opcode
+void Simulator::decode(){
+
+}
+
+//Executes instruction
+void Simulator::execute(){
+
+}
+
+//Set CI to content of store location
+void Simulator::JMP(){
+
+}
+
+//Add content of store location to CI
+void Simulator::JRP(){
+
+}
+
+//Load accumulator with negative form of store content 
+void Simulator::LDN(){
+
+}
+
+//Copy accumulator to store location
+void Simulator::STO(){
+
+}
+
+//Subtract content of store location from accumulator 
+void Simulator::SUB(){
+
+}
+
+//Increment CI if Accumulator value negative otherwise do nothing
+void Simulator::CMP(){
+
+}
+
+//Halts machine
+void Simulator::STP(){
+
+}
+
+//Displays memory state
+void Simulator::display_everything(){
+
+}
+
+
+
+
+
+void run(){
+    displayMenu();
+
+	//Create new instance of simulator
+	// Simulator sim;
+
+	//Should loop until STP function is recieved
+	// while (halted != true)
+	// {
+	// 	sim.increment_CI();
+	// 	sim.fetch();
+	// 	sim.decode();
+	// 	sim.execute();
+	// 	sim.display_everything();
+	// }
+}
 
 int main()
 {
-	displayMenu();
-
-    return 0;
+	run();
+	return 0;
 }
+
+
 
 void displayMenu()
 {
