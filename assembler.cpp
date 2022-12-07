70cd68489baf1eb2afd9f30783b5fceb8045c5f0
Kayee Liu <91502461+kayeeliu3@users.noreply.github.com>
2022-12-07 16:02:46 +0000

    initial: template program matching header

--- assembler.cpp
+++ assembler.cpp
@@ -1,12 +1,60 @@
 /*
+    ASSEMBLER PROGRAM
 	GROUP 1 MANCHESTER BABY SIMULATOR
 	Names: Finnbar Home, Abz Mohamed, Samuel Tweedie, Oliver Shearer, John Nicol, Kayee Liu
 	Module Code: AC21009
 */
-#include<iostream>
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include "assembler.h"
 using namespace std;
+
+// Set instruction set to either include the additional instructions
+void Assembler::setInstructions()
+{
+
+}
+
+void Assembler::displayOutput()
+{
+
+}
+
+// Grab corresponding opcode for mnemonic
+int Assembler::mnemonicToOpcode(string mnemonic)
+{
+
+}
+
+// Clean input file to assemble
+void Assembler::formatInput()
+{
+
+}
+
+// Process file for variables and instructions
+void Assembler::assemble()
+{
+
+}
+
+// Write output machine code into a text file
+void Assembler::writeToFile()
+{
+
+}
+
+void displayMenu()
+{
+
+}
 
 int main()
 {
-	
+    //displayMenu();
+    return 0;
 }
